1cf3dcf2263fdbe59f49ea712464a03ae1fc671e
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2022-10-06 17:42:42 -0300

    Agregado estilo al formulario

--- src/routes/Location/CreateLocation.jsx
+++ src/routes/Location/CreateLocation.jsx
@@ -35,26 +35,33 @@ const CreateLocation = () => {
             .catch( (error) => console.log(error))
     }
   return (
-    <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder='Nombre de la locacion'
-                {...register('locationName', { required: 'Debe ingresar un nombre' })}
-            />
-            <p>{errors.locationName?.message}</p>
+    <>
+        
+        <div className='form-container'>
+            <h1 className='create-location-title'>Generate a new location card</h1>
+            <p className='description'>
+                You can add a new location card putting its coordinates (latitude and longitude) and its name.
+            </p>
+            <form className='form' onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder='Nombre de la locacion'
+                    {...register('locationName', { required: 'Debe ingresar un nombre' })}
+                />
+                <p>{errors.locationName?.message}</p>
 
-            <input type="text" placeholder='Latitud'
-                {...register('lat', { required: 'Debe ingresar una latitud' })}
-            />
-            <p>{errors.lat?.message}</p>
+                <input type="text" placeholder='Latitud'
+                    {...register('lat', { required: 'Debe ingresar una latitud' })}
+                />
+                <p>{errors.lat?.message}</p>
 
-            <input type="text" placeholder='Longitud'
-                {...register('lon', { required: 'Debe ingresar una longitud' })}
-            />
-            <p>{errors.lon?.message}</p>
+                <input type="text" placeholder='Longitud'
+                    {...register('lon', { required: 'Debe ingresar una longitud' })}
+                />
+                <p>{errors.lon?.message}</p>
 
-            <button type='submit'>Crear Locacion</button>
-        </form>
-    </div>
+                <button class='btn' type='submit'>Create new card</button>
+            </form>
+        </div>
+    </>
   )
 }
 
